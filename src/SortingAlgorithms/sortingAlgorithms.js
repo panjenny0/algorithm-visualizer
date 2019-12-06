c0ea07344029b3d93d73c89c8cb838659d853589
JennyP <panjenny0@gmail.com>
2019-12-05 23:22:11 -0500

    added new js files for implementing sorting algorithms

---
+++ src/SortingAlgorithms/sortingAlgorithms.js
@@ -0,0 +1,21 @@
+export const mergeSort = array => {
+    if (array.length === 1) return array;
+    const mid = Math.floor(array.length / 2);
+    // slice returns the selected elements in an array into a new array object `slice(start, end)`
+    // if either start or end are omitted, method uses 0 as start of array and end of array as the end
+    const firstHalf = mergeSort(array.slice(0, mid));
+    const secondHalf = mergeSort(array.slice(mid));
+    const sorted = [];
+    let i = 0,
+        j = 0;
+    while (i < firstHalf.length && j < secondHalf.length) {
+        if (firstHalf[i] < secondHalf[j]) {
+            sorted.push(firstHalf[i++]);
+        } else {
+            sorted.push(secondHalf[j++]);
+        }
+    }
+    while (i < firstHalf.length) sorted.push(firstHalf[i++]);
+    while (j < secondHalf.length) sorted.push(secondHalf[j++]);
+    return sorted
+};
